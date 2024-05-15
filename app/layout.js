6d2a056c00140d0692bd4d9469875bb9aa5ae11d
Joyci <xxjoycixx@gmail.com>
2024-05-15 14:35:07 -0300

    upload

--- app/layout.js
+++ app/layout.js
@@ -4,20 +4,21 @@ import "./globals.scss";
 
 export const metadata = {
   title: "Comunidade respira | Alê Montezano",
-  description: "Aprenda técnicas de Breathwork capazes de te ajudar no controle das emoções, na redução da ansiedade, do estresse, melhora do sono e outros benefícios. Você será guiado por uma mentora por essa jornada de autoconhecimento e busca por bem-estar.",
+  description: "Aprenda técnicas de Breathwork capazes de te ajudar no controle das emoções, na redução da ansiedade, do estresse, na melhora do sono e outros benefícios. Você será guiado por uma mentora por essa jornada de autoconhecimento e busca por bem-estar.",
   keywords: ['breathwork', 'trabalho de respiração', 'respiração', 'respiração consciente', 'redução da ansiedade', 'melhora do sono', 'saúde'],
   openGraph: {
     title: 'Comunidade Respira | Alê Montezano',
-    description: 'Aprenda técnicas de Breathwork capazes de te ajudar no controle das emoções, na redução da ansiedade, do estresse, melhora do sono e outros benefícios. Você será guiado por uma mentora por essa jornada de autoconhecimento e busca por bem-estar.',
+    description: 'Aprenda técnicas de Breathwork capazes de te ajudar no controle das emoções, na redução da ansiedade, do estresse, na melhora do sono e outros benefícios. Você será guiado por uma mentora por essa jornada de autoconhecimento e busca por bem-estar.',
     url: 'https://www.alemontezano.com',
     siteName: 'Trote Solidário UFV',
     locale: 'pt_BR',
     type: 'website',
+    generator: 'Next.js',
   },
   twitter: {
     card: 'summary_large_image',
     title: 'Comunidade Respira | Alê Montezano',
-    description: 'Aprenda técnicas de Breathwork capazes de te ajudar no controle das emoções, na redução da ansiedade, do estresse, melhora do sono e outros benefícios. Você será guiado por uma mentora por essa jornada de autoconhecimento e busca por bem-estar.',
+    description: 'Aprenda técnicas de Breathwork capazes de te ajudar no controle das emoções, na redução da ansiedade, do estresse, na melhora do sono e outros benefícios. Você será guiado por uma mentora por essa jornada de autoconhecimento e busca por bem-estar.',
   },
   verification: {
     google: 'google',
