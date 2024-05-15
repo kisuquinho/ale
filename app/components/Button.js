9fd8ffaf8eec7b19aa42c98a2c30484224f4b3f8
Joyci <xxjoycixx@gmail.com>
2024-05-15 10:45:55 -0300

    upload

--- app/components/Button.js
+++ app/components/Button.js
@@ -5,6 +5,6 @@ export default function Button({ children, section, href, target }) {
     const className = `${styles.btn} + ${styles[section]}`
 
     return (
-        <a className={className} href="https://pay.infinitepay.io/alemontezano/482,46" target="_blank" rel="noopener noreferrer">{children}</a>
+        <a className={className} href="https://pag.ae/7-wGSuFr2" target="_blank" rel="noopener noreferrer">{children}</a>
     )
 }
